78548914605fa51af55c1a1973b87e5b8b23cef1
Zaid9498 <136815199+Zaid9498@users.noreply.github.com>
2023-08-25 23:43:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const sizeInfoDiv = document.getElementById("sizeInfo");
+const sizeText = document.createElement("h1");
+
+function updateSizeInfo() {
+    sizeText.textContent = `Width: ${window.innerWidth} Height: ${window.innerHeight}`;
+}
+
+window.addEventListener("resize", updateSizeInfo);
+
+sizeInfoDiv.appendChild(sizeText);
+
+// Initial update
+updateSizeInfo();
